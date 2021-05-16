0c94cde83492a1c2a2ca5dbb270367ff787c606b
jmart6784 <jmartinez6784@yahoo.com>
2021-05-16 13:51:17 -0700

    Added nav bar

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
+import Nav from "./components/Nav";
 
 const Routes = () => {
   return (
     <HashRouter>
+      <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/projects" component={Projects} />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,93 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
-const Nav = () => {};
+const Nav = () => {
+  const location = useLocation();
+  let navLinks;
+  let mobileLinks;
+
+  if (location.pathname === "/") {
+    navLinks = (
+      <div id="inner-div">
+        <button className="home-link nav-link">Home</button>
+        <button className="about-link nav-link">About</button>
+        <button className="skills-link nav-link">Skills</button>
+        <button className="projects-link nav-link">Projects</button>
+        <button className="contact-link nav-link">Contact</button>
+      </div>
+    );
+
+    mobileLinks = (
+      <div className="mobile-inner-div">
+        <button className="home-link mob-nav-link">Home</button>
+        <button className="about-link mob-nav-link">About</button>
+        <button className="skills-link mob-nav-link">Skills</button>
+        <button className="projects-link mob-nav-link">Projects</button>
+        <button className="contact-link mob-nav-link">Contact</button>
+      </div>
+    );
+  } else {
+    navLinks = (
+      <div id="inner-div">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/#about-me-div" className="nav-link">
+          About
+        </Link>
+        <Link to="/#skills-container" className="nav-link">
+          Skills
+        </Link>
+        <Link to="/#projects-container" className="nav-link">
+          Projects
+        </Link>
+        <Link to="/#contact-container" className="nav-link">
+          Contact
+        </Link>
+      </div>
+    );
+
+    mobileLinks = (
+      <div className="mobile-inner-div">
+        <Link to="/" className="mob-nav-link">
+          Home
+        </Link>
+        <Link to="/#about-me-div" className="mob-nav-link">
+          About
+        </Link>
+        <Link to="/#skills-container" className="mob-nav-link">
+          Skills
+        </Link>
+        <Link to="/#projects-container" className="mob-nav-link">
+          Projects
+        </Link>
+        <Link to="/#contact-container" className="mob-nav-link">
+          Contact
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div id="nav-parent">
+        <div id="nav-dark-blur"></div>
+
+        <div id="nav-container">{navLinks}</div>
+      </div>
+
+      <div className="mobile-nav-parent">
+        <div id="mobile-nav-container">
+          <div className="mobile-header">
+            <button id="mobile-ham">☰</button>
+            <div className="dark-blur"></div>
+          </div>
+
+          {mobileLinks}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
