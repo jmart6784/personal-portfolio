9d0bdb25220f8ed61a207e4644927e962eb8d8b8
jmart6784 <jmartinez6784@yahoo.com>
2021-06-08 23:40:15 -0700

    Added Postgres Skill to list

--- src/components/skills-data.js
+++ src/components/skills-data.js
@@ -6,13 +6,14 @@ const skillsData = [
   { id: 5, name: "JavaScript", image: "js.png" },
   { id: 6, name: "Ruby", image: "ruby.png" },
   { id: 7, name: "Rails", image: "rails.png" },
-  { id: 8, name: "Jest", image: "jest.png" },
-  { id: 9, name: "Git", image: "git.png" },
-  { id: 10, name: "Github", image: "github.png" },
-  { id: 11, name: "Heroku", image: "heroku.png" },
-  { id: 12, name: "Webpack", image: "webpack.png" },
-  { id: 13, name: "NPM", image: "npm.png" },
-  { id: 14, name: "Yarn", image: "yarn.png" },
+  { id: 8, name: "PostgreSQL", image: "postgres.svg" },
+  { id: 9, name: "Jest", image: "jest.png" },
+  { id: 10, name: "Git", image: "git.png" },
+  { id: 11, name: "Github", image: "github.png" },
+  { id: 12, name: "Heroku", image: "heroku.png" },
+  { id: 13, name: "Webpack", image: "webpack.png" },
+  { id: 14, name: "NPM", image: "npm.png" },
+  { id: 15, name: "Yarn", image: "yarn.png" },
 ];
 
 export default skillsData;
