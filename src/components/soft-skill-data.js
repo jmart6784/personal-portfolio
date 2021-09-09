1e236e9462b052631fce122a8dce391cafb1d7b9
jmart6784 <jmartinez6784@yahoo.com>
2021-09-09 16:40:18 -0700

    Added more skills data and images

--- src/components/soft-skill-data.js
+++ src/components/soft-skill-data.js
@@ -1,5 +1,18 @@
 const softSkillsData = [
-  { id: 1, name: "Persistence", image: "persistence.png" },
+  { id: 1, name: "Persistent", image: "persistent.png" },
+  { id: 2, name: "Hard working", image: "hard-working.png" },
+  { id: 3, name: "Empathetic", image: "empathy.png" },
+  { id: 4, name: "Problem Solver", image: "problem-solver.png" },
+  { id: 5, name: "Adaptable", image: "adapt.png" },
+  { id: 6, name: "Critical thinker", image: "critical-thinker.png" },
+  { id: 7, name: "Patient", image: "patience.png" },
+  { id: 8, name: "Passion", image: "passion.png" },
+  { id: 9, name: "Responsible", image: "resp.png" },
+  { id: 10, name: "Creative", image: "creative.png" },
+  { id: 11, name: "Honest", image: "honest.png" },
+  { id: 12, name: "Helpful", image: "helpful.png" },
+  { id: 13, name: "Organized", image: "organized.png" },
+  { id: 14, name: "Technical", image: "technical.png" }
 ];
 
 export default softSkillsData;
